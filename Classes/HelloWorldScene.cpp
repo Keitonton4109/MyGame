36b02528267dc8cbeeb9d591d84f3927937ffb9b
keitonton4109 <keitonton4109@gmail.com>
2019-06-24 14:25:55 +0900

    イージングアクション

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -108,9 +108,15 @@ bool HelloWorld::init()
 	this->addChild(sprite);
 	//表示座標を指定
 	sprite->setPosition(Vec2(visibleSize.width/2.0f,visibleSize.height/2.0f));
-
-	//MoveBy*action1 = MoveBy::create(1.0f, Vec2(200, 100));
+	//ノードを指定した座標に移動させる
+	MoveBy*action1 = MoveBy::create(1.0f, Vec2(400, 200));
+	//アクションに対して、イージングを付けたアクションを生成する
+	EaseInOut*action2 = EaseInOut::create(action1, 2.0f);
+	//イージング付きアクションの実行
+	sprite->runAction(action2);
+	//ノードを拡大/縮小する
 	//ScaleBy*action1 = ScaleBy::create(1.0f, 5.0f);
+	//ノードをジャンプさせる
 	//JumpTo*action1 = JumpTo::create(1.0f, Vec2(200, 100), 500.0f, 6);
 	////ベジェ曲線
 	//ccBezierConfig conf;
@@ -118,8 +124,9 @@ bool HelloWorld::init()
 	//conf.controlPoint_2 = Vec2(1000, 700);
 	//conf.endPosition = Vec2(1200, 360);
 	//BezierTo*action1 = BezierTo::create(2.0f, conf);
-	sprite->setOpacity(0);
-	FadeIn*action1 = FadeIn::create(1.0f);
+	//ノードをフェードイン/フェードアウトさせる
+	//sprite->setOpacity(0);
+	//FadeIn*action1 = FadeIn::create(1.0f);
 	sprite->runAction(action1);
 	//sprite->setScale(5.0f);
 	//sprite->setAnchorPoint(Vec2(0, 1));
