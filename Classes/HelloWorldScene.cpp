01364dc5f3f5d97d82cfcaa81f856a076b8d67b7
keitonton4109 <keitonton4109@gmail.com>
2019-07-01 13:53:06 +0900

    Repeat　サンプル

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -104,18 +104,28 @@ bool HelloWorld::init()
 	Sprite*spr = Sprite::create("HelloWorld.png");
 	this->addChild(spr);
 
-	//アクション1の作成
-	MoveTo*moveTo = MoveTo::create(2.0f, Vec2(600.0f, 300.0f));
-	//アクション2の作成
-	JumpTo*jumpTo = JumpTo::create(1.0f, Vec2(200.0f, 200.0f), 300.0f, 2);
+	JumpBy*jumpBy = JumpBy::create(0.5f, Vec2(100.0f, 100.0f),100.0f,1);
 
-	TintTo*tintTo = TintTo::create(1.0f, Color3B(255, 255, 0));
-	//同時発動アクションの作成
-	Spawn*spawn = Spawn::create(moveTo,tintTo,nullptr);
-	//連続アクションの生成
-	Sequence*seq = Sequence::create(moveTo, spawn, nullptr);
-	//連続アクションの実行
-	spr->runAction(seq);
+	MoveTo*moveTo = MoveTo::create(2.0f, Vec2(600.0f, 300.0f));
+
+	Sequence*seq = Sequence::create(jumpBy, moveTo, nullptr);
+	//繰り返しアクションの生成
+	Repeat*rep= Repeat::create(seq, 10);
+
+	spr->runAction(rep);
+
+	////アクション1の作成
+	//MoveTo*moveTo = MoveTo::create(2.0f, Vec2(600.0f, 300.0f));
+	////アクション2の作成
+	//JumpTo*jumpTo = JumpTo::create(1.0f, Vec2(200.0f, 200.0f), 300.0f, 2);
+
+	//TintTo*tintTo = TintTo::create(1.0f, Color3B(255, 255, 0));
+	////同時発動アクションの作成
+	//Spawn*spawn = Spawn::create(moveTo,tintTo,nullptr);
+	////連続アクションの生成
+	//Sequence*seq = Sequence::create(moveTo, spawn, nullptr);
+	////連続アクションの実行
+	//spr->runAction(seq);
 
 	//updateが呼び出されるようにする
 	//this->scheduleUpdate();
